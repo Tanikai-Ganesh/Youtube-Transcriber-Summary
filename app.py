aa884471a6072bf4ff8dcdaca4cb6f43089a4e30
Tanikai-Ganesh <tanikaiceg02@gmail.com>
2025-04-11 13:13:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ youtube_link = st.text_input("Enter YouTube Video Link:")
 if youtube_link:
     video_id = youtube_link.split("=")[1]
     print(video_id)
-    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_container_width=True)
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
 if st.button("Get Detailed Notes"):
     transcript_text=extract_transcript_details(youtube_link)
